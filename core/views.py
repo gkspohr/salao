2f440cf36d2864ccc51e49dd9d4baa4057f1411c
Guilherme Kohl Spohr <gkspohr@gmail.com>
2022-10-13 17:52:52 -0300

    Organizacao Projeto

--- core/views.py
+++ core/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render
 from .forms import ClienteForm, FuncionarioForm
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index/index.html')
 
 def login(request):
-    return render(request, 'login.html')    
+    return render(request, 'login/login.html')    
 
 def agenda(request):
     return render(request, 'agenda.html')    
